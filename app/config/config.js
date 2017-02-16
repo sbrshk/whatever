d43166cd7df22054cd555f57c8423a86db6f72ac
Sophya <alan.turing39@gmail.com>
2017-02-16 23:11:39 +0300

    config

--- app/config/config.js
+++ app/config/config.js
@@ -3,6 +3,7 @@ const Config = reuire('electron-config')
 const config = new Config({
     defaults: {
         autoStart: false,
+        backgroundMode: false,
         colorTheme: "classic"
     }
 })
